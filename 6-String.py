3410a51d1b617cf63c27bc4e5668d734c0e9f491
krishnachaudhary14555 <krishnachaudhary3942@gmail.com>
2026-07-01 19:10:11 +0530

    Introduction to String , String Indexing ,SLicing

---
+++ 6-String.py
@@ -0,0 +1,25 @@
+
+''' String -> A string is s sequence of characters enclosed single , double, or triple quotes . It is immutable '''
+
+# String Indexing 
+name = "Krishna"
+# print(name)
+# print(name[0]) # K
+# print(name[-1]) # a  Or we can  also write print(name[6])  --> it will give the same result
+
+
+# # String Slicing  : string[start : end ]
+
+product = "LaptopModel2025"
+# print(product[6 : 11]) # Model 
+
+# # # Backward indexing 
+print(product[-4 : ]) # 2025
+
+# Skip Text  "LaptopModel2025"
+# print(product[0 : 16 : 2])      #LpoMdl05
+
+'''Reverse String '''
+
+Product1 = "New Model Laptop 2026" 
+print(Product1[ : : -1])  #6202 potpaL ledoM weN
